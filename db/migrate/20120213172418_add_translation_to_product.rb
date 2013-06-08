d693d0ded0b853556521a392a29d3bf456a63627
Fareed Dudhia <fareeddudhia@googlemail.com>
2013-06-08 18:31:37 +0000

    changed them back

--- db/migrate/20120213172418_add_translation_to_product.rb
+++ db/migrate/20120213172418_add_translation_to_product.rb
@@ -1,8 +1,8 @@
 class AddTranslationToProduct < ActiveRecord::Migration
   def up
-  Spree::Config[:track_inventory_levels] = false
-  Spree::Product.create_translation_table! :name => :string, :description => :text, :meta_description => :string, :meta_keywords => :string
-  Spree::Config[:track_inventory_levels] = true
+      Spree::Config[:track_inventory_levels] = false
+      Spree::Product.create_translation_table! :name => :string, :description => :text, :meta_description => :string, :meta_keywords => :string
+      Spree::Config[:track_inventory_levels] = true
   end
 
   def down

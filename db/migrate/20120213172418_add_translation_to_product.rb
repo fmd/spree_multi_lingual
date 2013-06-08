aec72bcfe9eaa259751f78e4a661cf561bca4402
Fareed Dudhia <fareeddudhia@googlemail.com>
2013-06-08 17:28:55 +0000

    migrations changes

--- db/migrate/20120213172418_add_translation_to_product.rb
+++ db/migrate/20120213172418_add_translation_to_product.rb
@@ -1,7 +1,10 @@
 class AddTranslationToProduct < ActiveRecord::Migration
   def up
-    Spree::Product.create_translation_table! :name => :string, :description => :text, :meta_description => :string, :meta_keywords => :string
+  Spree::Config[:track_inventory_levels] = false
+  Spree::Product.create_translation_table! :name => :string, :description => :text, :meta_description => :string, :meta_keywords => :string
+  Spree::Config[:track_inventory_levels] = true
   end
+
   def down
     Spree::Product.drop_translation_table!
   end  
--- db/migrate/20120309094246_migrate_data_for_products.rb
+++ db/migrate/20120309094246_migrate_data_for_products.rb
@@ -2,7 +2,9 @@ class MigrateDataForProducts < ActiveRecord::Migration
   def up
     Spree::Product.transaction do
       Spree::Product.find_each do |product|
+        Spree::Config[:track_inventory_levels] = false
         product.update_attributes!(product.untranslated_attributes, :without_protection => true)
+        Spree::Config[:track_inventory_levels] = true
       end
     end
   end
